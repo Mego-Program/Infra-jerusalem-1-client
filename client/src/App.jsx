359fed4ca67a068727c85b9a4d9a63902d812a0b
Simcha Sucot <simchasucot@gmail.com>
2023-10-30 14:39:13 +0200

    Update of the basic NAV

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+//pages
+// The pages need to be prepared and updated here
+
+//Layout
+import AppLayout from "./components/AppLayout.jsx";
 
 export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<p>Dashboard</p>} />
+        <Route path="Projects" element={<p>Projects</p>} />
+        <Route path="Board" element={<p>Board </p>} />
+        <Route path="AddUser" element={<p>AddUser </p>} />
+        <Route path="Messages" element={<p>Messages </p>} />
+        <Route path="*" element={<p>NotFound </p>} />
+      </Route>
+    )
+  );
+
   return (
-    <div>App</div>
-  )
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
