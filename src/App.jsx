2223507912036e91b07813192de2f1fc5f91a645
shalom oberlander <103103sh@gmail.com>
2023-11-02 14:31:04 +0200

    Add the pages of sign in and sign up

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import {
 
 //Layout
 import AppLayout from "./components/AppLayout.jsx";
+import SignUp from './components/login/signup.jsx'
+import SignIn from "./components/login/signin.jsx";
+
 
 export default function App() {
   const router = createBrowserRouter(
@@ -21,13 +24,15 @@ export default function App() {
         <Route path="Board" element={<p>Board </p>} />
         <Route path="AddUser" element={<p>AddUser </p>} />
         <Route path="Messages" element={<p>Messages </p>} />
+        <Route path="signup" element={<SignUp/>} />
+        <Route path="signin" element={<SignIn/>} />
         <Route path="*" element={<p>NotFound </p>} />
       </Route>
     )
   );
 
   return (
-    <div>
+    <div style={{ backgroundColor: '#21213E'}}>
       <RouterProvider router={router} />
     </div>
   );
