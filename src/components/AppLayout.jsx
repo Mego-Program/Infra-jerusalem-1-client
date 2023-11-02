279e0dc101926d92d60e0d55eb735e9b2a768ac9
Simcha Sucot <simchasucot@gmail.com>
2023-11-02 12:10:26 +0200

    dlete the tailwind

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -4,37 +4,37 @@ import "./AppLayout.css";
 
 export default function AppLayout() {
   return (
-    <div className="w-[270px] h-[1323px] bg-slate-900 container None" >
+    <div>
       <nav>
         <NavLink to="/">
-        <div className="w-[246px] h-[66px] bg-amber-400 rounded-[10px] shadow" >
-            <img src="/layoutIcons/dashboard.svg" alt="" className="w-5 h-[18px] relative"/>
-            <div className="text-white text-[17px] font-semibold font-['Poppins']">Dashboard</div>
+        <div>
+            <img src="/layoutIcons/dashboard.svg" alt="" />
+            <div>Dashboard</div>
           </div>
         </NavLink>
         <NavLink to="Projects">
           <div>
             <img src="/layoutIcons/projects.svg" alt="" />
             {/*The SVG is missing here */}
-            <div className="text-white text-[17px] font-semibold font-['Poppins']">Projects</div>
+            <div>Projects</div>
           </div>
         </NavLink>
         <NavLink to="Board">
           <div>
             <img src="/layoutIcons/board.svg" alt="" />
-            <div className="text-white text-[17px] font-semibold font-['Poppins']">Board</div>
+            <div>Board</div>
           </div>
         </NavLink>
         <NavLink to="AddUser">
           <div>
             <img src="/layoutIcons/addUser.svg" alt="" />
-            <div className="text-white text-[17px] font-semibold font-['Poppins']">Add User</div>
+            <div>Add User</div>
           </div>
         </NavLink>
         <NavLink to="Messages">
           <div>
             <img src="/layoutIcons/messages.svg" alt="" />
-            <div className="text-white text-[17px] font-semibold font-['Poppins']">Messages</div>
+            <div>Messages</div>
           </div>
         </NavLink>
       </nav>
