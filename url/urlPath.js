a8a691fd9668f00ad6aa25f2891c6f9415e9ab43
Moshe Brizel <brizelmoshe@gmail.com>
2023-11-08 12:06:01 +0200

    add new url

--- url/urlPath.js
+++ url/urlPath.js
@@ -1,3 +1,4 @@
-const urlPage = "http://localhost:5050/"
+const urlPage = "http://localhost:5050/";
+// const urlPage = "https://infra-jerusalem-1-server.vercel.app/"
 
-export default urlPage 
+export default urlPage;
