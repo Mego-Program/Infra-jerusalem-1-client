3bd1396514e3892e2333a68281883a5ed7b9bbbd
Moshe Brizel <brizelmoshe@gmail.com>
2023-11-15 13:59:52 +0200

    add the atom user.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "userAtom",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./UserAtom": "./src/atoms/atomUser",
+      },
+      shared: {
+        jotai: {
+          singleton: true, // Set this to true if 'jotai' is a singleton
+        },
+      },
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
