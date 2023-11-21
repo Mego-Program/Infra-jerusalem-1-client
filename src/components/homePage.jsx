ee2667306e7e009cb37518d7d0cee82407e2b946
דוד הכהן <dudihakoen1@gmail.com>
2023-11-21 22:31:59 +0200

    create home page

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -8,6 +8,9 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Link } from "react-router-dom";
 import theme
  from "../theme";
+
+
+
 function Home(props) {
  
   return (
@@ -28,25 +31,59 @@ function Home(props) {
         <CssBaseline />
         <Box
           sx={{
-            marginTop: 8,
+            marginTop: 4, 
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+
+          }}
+        >
+          <img
+            src="\logo\logo.png"
+            alt="Logo"
+            style={{ width: "2000px", height: "200px", marginBottom: "16px" }}
+          />
+          </Box>
+          <Box
+          sx={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
           }}
         >
-          <Typography
+{/* השארתי בהערה למקרה שירצו לעשות כותרת */}
+          {/* <Typography
             component="h1"
-            variant="h5"
+            variant="h2"
             sx={{
               color: "yelow.main",
+              textAlign: "center",
+              fontSize:'50px'
             }}
           >
-            Welcome to הכנפיים של קורמבי {props.companyName}
+            mego project {props.companyName}
+          </Typography> */}
+          <Typography
+            variant="body2" 
+            sx={{
+              color: "yelow.main",
+              textAlign: "center",
+              marginTop: 2,
+              fontSize:'30px',
+              fontFamily: "-moz-initial",
+              fontStyle: "italic",
+              textAlign: "left",
+              marginRight: 5,
+
+ 
+            }}
+          >
+            A system for managing tasks, tracking them and interaction between employees
           </Typography>
           <Box
             sx={{
               display: "flex",
-              flexDirection: "column",
+              flexDirection: "row",
               alignItems: "center",
               marginTop: 4,
             }}
@@ -55,10 +92,21 @@ function Home(props) {
               <Button
                 fullWidth
                 variant="contained"
-                sx={{ mb: 2 }}
+                sx={{ flex: 1, marginRight: 1}}
                 color="yelow"
               >
                 Log In
+              </Button>
+            </Link>
+          
+            <Link to="./signup" style={{ textDecoration: "none" }}>
+              <Button
+                fullWidth
+                variant="contained"
+                sx={{ flex: 1, marginLeft: 1}}
+                color="yelow"
+              >
+                Sign up
               </Button>
             </Link>
           </Box>
